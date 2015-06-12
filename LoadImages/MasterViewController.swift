f3d5bffb6cef0067f6f2c2774afd2816c6de1727
Roman Salabay <roman.salabay@lezgro.com>
2015-06-12 20:29:09 +0300

    Fixed crash and auto layout issue.

--- LoadImages/MasterViewController.swift
+++ LoadImages/MasterViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class MasterViewController: UIViewController {
+class MasterViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   @IBOutlet weak var tableView: UITableView!
   
-  var objects = ["http://lh5.ggpht.com/_hepKlJWopDg/TB-_WXikaYI/AAAAAAAAElI/715k4NvBM4w/s144-c/IMG_0075.JPG",
-    "http://lh4.ggpht.com/_4f1e_yo-zMQ/TCe5h9yN-TI/AAAAAAAAXqs/8X2fIjtKjmw/s144-c/IMG_1786.JPG",
+  let objects = ["http://lh5.ggpht.com/_hepKlJWopDg/TB-_WXikaYI/AAAAAAAAElI/715k4NvBM4w/s144-c/IMG_0075.JPG",
+    "http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg",
     "http://lh3.ggpht.com/_GEnSvSHk4iE/TDSfmyCfn0I/AAAAAAAAF8Y/cqmhEoxbwys/s144-c/_MG_3675.jpg",
     "http://lh6.ggpht.com/_ZN5zQnkI67I/TCFFZaJHDnI/AAAAAAAABVk/YoUbDQHJRdo/s144-c/P9250508.JPG",
     "http://lh4.ggpht.com/_XjNwVI0kmW8/TCOwNtzGheI/AAAAAAAAC84/SxFJhG7Scgo/s144-c/0014.jpg",
@@ -29,10 +29,11 @@ class MasterViewController: UIViewController {
     "http://lh6.ggpht.com/_ZN5zQnkI67I/TCFFZaJHDnI/AAAAAAAABVk/YoUbDQHJRdo/s144-c/P9250508.JPG",
     "http://lh4.ggpht.com/_XjNwVI0kmW8/TCOwNtzGheI/AAAAAAAAC84/SxFJhG7Scgo/s144-c/0014.jpg",
     "http://lh6.ggpht.com/_Nsxc889y6hY/TBp7jfx-cgI/AAAAAAAAHAg/Rr7jX44r2Gc/s144-c/IMGP9775a.jpg"]
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 128
   }
   
   override func didReceiveMemoryWarning() {
@@ -49,39 +50,45 @@ class MasterViewController: UIViewController {
     return 200
   }
   
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! TableViewCell
     let image_url = NSURL(string: objects[indexPath.row])
     let url_request = NSURLRequest(URL: image_url!)
     
-    cell.mainImage?.setImageWithURLRequest(url_request,
-      placeholderImage: nil,
-      success: {
-        [weak cell, weak self]  (request:NSURLRequest!,response:NSHTTPURLResponse!, image:UIImage!) -> Void in
-        if let cell_for_image = cell {
-          cell_for_image.mainImage.image = image
-          cell_for_image.imageHeight.constant = image.size.height
-          cell_for_image.delegate?.tableViewCell(cell_for_image)
-        }
-      }, failure: {
-        [weak cell]
-        (request:NSURLRequest!,response:NSHTTPURLResponse!, error:NSError!) -> Void in
-        if let cell_for_image = cell {
-          cell_for_image.mainImage!.image = nil
-          cell_for_image.setNeedsLayout()
-        }
-      })
+    if let image = cell.mainImage?.dynamicType.sharedImageCache().cachedImageForRequest(url_request) {
+        cell.mainImage?.image = image
+        cell.imageHeight?.constant = image.size.height
+    }else {
+        cell.mainImage?.setImageWithURLRequest(url_request,
+            placeholderImage: nil,
+            success: {
+                [weak cell, weak self]  (request:NSURLRequest!,response:NSHTTPURLResponse!, image:UIImage!) -> Void in
+                if let cell_for_image = cell {
+                    cell_for_image.mainImage.image = image
+                    cell_for_image.imageHeight.constant = image.size.height
+                    cell_for_image.delegate?.tableViewCell(cell_for_image)
+                }
+            }, failure: {
+                [weak cell]
+                (request:NSURLRequest!,response:NSHTTPURLResponse!, error:NSError!) -> Void in
+                if let cell_for_image = cell {
+                    cell_for_image.mainImage!.image = nil
+                    cell_for_image.setNeedsLayout()
+                }
+            })
+    }
+    
     cell.delegate = self
     return cell
   }
 }
-
 extension MasterViewController: TableViewCellDelegate {
   func tableViewCell(cell: UITableViewCell) {
     tableView.beginUpdates()
-    if let cellIndexPath = tableView.indexPathForRowAtPoint(cell.center) {
-      tableView.reloadRowsAtIndexPaths([cellIndexPath], withRowAnimation: .Fade)
-    }
     tableView.endUpdates()
   }
 }
